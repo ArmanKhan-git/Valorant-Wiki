a57cb03143881c87edf0c09a44253ac469c7d165
Arman Khan <armankkhan14@gmail.com>
2025-06-28 18:26:46 +0530

    Agentdetails.jsx changes overflowhidden below button div

--- src/Agentdetails.jsx
+++ src/Agentdetails.jsx
@@ -13,7 +13,7 @@ const agent = agentData.find(
     }
   return (<>
         <button onClick={()=>navigate(-1)} className=' m-4 border py-1 px-2 rounded-2xl bg-white text-black font-extrabold  hover:cursor-pointer'>&lt;-back </button>
-      <div className='container relative mx-auto p-2 '>
+      <div className='container relative  pl-2 max-w-full  overflow-x-hidden'>
       <h1 className='text-7xl font-bold mb-4 z-10 relative  '>{agent.name}</h1>
         <img className='absolute z-0 right-0 mt-20 scale-[180%] opacity-50 ' src={agent.img} alt="" />
       <span className='text-2xl relative z-10 border rounded-2xl px-2 font-bold opacity-60'>{agent.role}</span>
@@ -29,7 +29,7 @@ const agent = agentData.find(
                 <div className="abilitiessection mt-5 gap-3 text-xl flex relative z-10 ">
                 <div key={index}>
                     
-                    <img className='w-20' src={ability.abimg} alt="" />
+                    <img className=' max-w-20' src={ability.abimg} alt="" />
                     </div>
                     <div>
                     <h1 className='font-bold z-10'>{ability[abilityname]}</h1>
@@ -40,7 +40,7 @@ const agent = agentData.find(
             )
         })
     ):(
-        <p className='text-gray-400 font-bold m-40 text-9xl'>No Data Found About the Agent</p>
+        <p className='text-gray-400 font-bold lg:m-40 text-9xl'>No Data Found About the Agent</p>
     )}
       </div>
     </div>
